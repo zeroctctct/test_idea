06e400235c64d7b81ccc9bf9bee085b44e6fce4f
zeroctctct <92285699+zeroctctct@users.noreply.github.com>
2022-08-17 20:54:28 +0800

    Update hellow.java
    第二次修改

--- src/main/java/hellow.java
+++ src/main/java/hellow.java
@@ -7,5 +7,6 @@ public class hellow {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("第一次修改");
+         System.out.println("第er次修改");
     }
 }
