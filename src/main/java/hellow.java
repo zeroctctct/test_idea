786e50923ebdf8ea50d663bee1e922894e89f4ea
ctcc <583996639@qq.com>
2022-08-17 20:25:47 +0800

    创建git

---
+++ src/main/java/hellow.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: ct
+ * @CreateTime: 08-17  20:24
+ * @Description: TODO
+ */
+public class hellow {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+    }
+}
